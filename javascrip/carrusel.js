bcf47283cef7a7f1d58f8452961617a6f9a35b85
Melany0103 <72016438@certus.edu.pe>
2022-07-01 03:44:45 -0500

    version40

---
+++ javascrip/carrusel.js
@@ -0,0 +1,13 @@
+document.addEventListener(`DOMContentLoaded`, () => {
+    const elementosCarousel = document.querySelectorAll(`.carousel`);
+    M.Carousel.init(elementosCarousel, {
+        duration: 150,
+        dist: -0,
+        shift: 5,
+        padding: 5,
+        numVisible: 3,
+        indicators: true,
+        noWrap: false,
+
+    });
+});
